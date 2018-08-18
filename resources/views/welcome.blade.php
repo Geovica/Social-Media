3553caaf40d7469ad191eb0da565962c003d7c14
Geovic Fagel <fgeovic@gmailcom>
2018-08-18 23:31:53 +0800

    add code

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+
+
+<div class="jumbotron text-center">
+        <h1 class="display-4">Welcome to Laravel</h1>
+       
+        <p>This is a simple Laravel Application</p>
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#" role="button">Login</a> <a class="btn btn-success btn-lg" href="#" role="button">Register</a>
+        </p>
+      </div>
+
+@endsection
